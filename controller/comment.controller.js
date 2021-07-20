f152400720decc27627e246dcbbaf393d0b22ac3
angel wang <13580376450@163.com>
2021-07-20 13:27:30 +0800

    评论接口完成

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -11,9 +11,28 @@ class commentController {
             await next()
         } catch (errMsg) {
             const error = new Error(errorType.SYSTEMERROR)
-            return ctx.app.emit('error', error, ctx, errMsg)
+            return ctx.app.emit('error', error, ctx, errMsg.message)
         }
-      
+    }
+    async reply(ctx, next) {
+        try {
+            const { commentId } = ctx.params
+            const { mommentId, content } = ctx.request.body
+            const { id: userid } = ctx.user
+            const result = await commentService.reply(commentId, mommentId, content, userid)
+            ctx.body = result
+            await next()
+        } catch (errMsg) {
+            const error = new Error(errorType.SYSTEMERROR)
+            return ctx.app.emit('error', error, ctx, errMsg.message)
+        }
+    }
+    async update(ctx, next) {
+        try {
+            
+        } catch (err) {
+            
+        }
     }
 }
 module.exports = new commentController()
--- router/comment.router.js
+++ router/comment.router.js
@@ -1,8 +1,13 @@
 const Router = require('koa-router')
 const { verifyToken } = require('../middleware/login.middleware')
-const { create } = require('../controller/comment.controller')
+const { create, reply } = require('../controller/comment.controller')
+const { verifyCommentId } = require('../middleware/comment.middleware')
+const { verifyPermission } = require('../middleware/comment.middleware')
+const { update } = require('../controller/comment.controller')
 
 const commentRouter = new Router({ prefix: '/comment' })
 
 commentRouter.post('/', verifyToken, create)
+commentRouter.post('/reply/:commentId', verifyToken, verifyCommentId ,reply)
+commentRouter.patch('/:commentId', verifyToken, verifyPermission, update)
  module.exports = commentRouter
